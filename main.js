40ab17dc60f4714e8338d6037d9f95d7c160da21
NijatH12 <nicath1712@mail.ru>
2024-04-30 12:42:12 +0400

    JavaScript

---
+++ main.js
@@ -0,0 +1,7 @@
+var address = prompt("Catdirilma addressini yoxlayin")
+if( address=="Yasamal" || address == "yasamal"|| address == "Bileceri"|| address == "bileceri"){
+    alert("Catdirilma var")
+}
+else{
+    alert("Catdirilma yoxdur")
+}
